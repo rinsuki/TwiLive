87d98ca5561e7664ac0f2dfd72314dfe68562d40
rinsuki <428rinsuki+git@gmail.com>
2019-09-08 11:56:07 +0900

    Initial Commit

---
+++ TwiLive/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TwiLive
+//
+//  Created by user on 2019/09/08.
+//  Copyright © 2019 rinsuki. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TwiLive/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TwiLive
+//
+//  Created by user on 2019/09/08.
+//  Copyright © 2019 rinsuki. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
